235478ad6f10a3b1b9f96ab11edcb865c84af0b8
simokd22 <simolkhalidi22@gmail.com>
2023-05-22 22:49:06 +0100

    modification des fichier migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Laravel\Fortify\Fortify;
+
 
 return new class extends Migration
 {
@@ -22,6 +24,13 @@ return new class extends Migration
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
+            $table->text('two_factor_secret')->nullable();
+
+            $table->text('two_factor_recovery_codes')->nullable();
+
+            if (Fortify::confirmsTwoFactorAuthentication()) {
+             $table->timestamp('two_factor_confirmed_at')->nullable();
+            }
         });
     }
 
